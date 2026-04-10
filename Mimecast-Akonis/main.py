d2f83e45a276becc21524ca05855c06b5dd9ca7d
Hakim <helkabouss@akonis.fr>
2026-04-10 10:26:11 +0200

    Add Mimecast-Akonis module with generated files

---
+++ Mimecast-Akonis/main.py
@@ -0,0 +1,5 @@
+from mimecast-akonis_modules import Mimecast-AkonisModule
+
+if __name__ == "__main__":
+    module = Mimecast-AkonisModule()
+    module.run()
---
+++ Mimecast-Akonis/mimecast-akonis_modules/__init__.py
@@ -0,0 +1,6 @@
+from sekoia_automation.module import Module
+from mimecast-akonis_modules.models import Mimecast-AkonisModuleConfiguration
+
+
+class Mimecast-AkonisModule(Module):
+    configuration: Mimecast-AkonisModuleConfiguration
